e989b86fbafd0db6f352bcca181abffa19030803
Tom Forbes <tom.forbes@wiz.io>
2026-01-17 23:06:50 +0000

    Fix parquet generation

--- export.sql
+++ export.sql
@@ -1,19 +1,71 @@
+SET enable_progress_bar = true;
+SET enable_progress_bar_print = true;
+SET progress_bar_time = 100;
+SET max_memory = '2GB';
+SET threads = 4;
 INSTALL sqlite;
 LOAD sqlite;
 
-copy
-(
+COPY (
     select region,
-       AvailabilityZone as availability_zone,
-       InstanceType     as instance_type,
-       SpotPrice::NUMERIC(10, 6) as spot_price, Timestamp ::TIMESTAMPTZ as timestamp
-    from read_json_auto(
-        'spot_price_data/*/*.jsonl.gz', hive_partitioning = true
+           AvailabilityZone as availability_zone,
+           InstanceType     as instance_type,
+           SpotPrice::NUMERIC(10, 6) as spot_price,
+           Timestamp::TIMESTAMP_S as timestamp
+    from read_json(
+        'spot_price_data/*/*.jsonl.gz',
+        columns = {AvailabilityZone: 'VARCHAR', InstanceType: 'VARCHAR', SpotPrice: 'VARCHAR', Timestamp: 'VARCHAR'},
+        hive_partitioning = true
     )
+ ) TO 'all.parquet'  (
+    FORMAT parquet
+    , COMPRESSION zstd
+);
+
+COPY
+(
+    select *
+    FROM 'all.parquet'
     ORDER BY region, availability_zone, instance_type, timestamp
 )
-to 'history.parquet' (COMPRESSION 'ZSTD');
+to 'history.parquet' (
+    FORMAT parquet
+    , COMPRESSION zstd
+    , COMPRESSION_LEVEL 9
+    , PRESERVE_ORDER true
+    , PER_THREAD_OUTPUT false
+    , OVERWRITE
+    , PARQUET_VERSION v2
+);
 
+COPY
+(
+    select *
+    FROM 'all.parquet'
+    WHERE timestamp >= (select max(timestamp) from 'all.parquet') - INTERVAL 90 DAY
+    ORDER BY region, availability_zone, instance_type, timestamp
+)
+to 'last-90-days.parquet' (
+FORMAT parquet
+    , COMPRESSION zstd
+    , COMPRESSION_LEVEL 9
+    , PRESERVE_ORDER true
+    , PER_THREAD_OUTPUT false
+    , OVERWRITE
+    , PARQUET_VERSION v2
+);
+
+SELECT pg_size_pretty(sum(total_compressed_size)::bigint) as size,
+    (SELECT count(*) FROM read_parquet('history.parquet')) as rows,
+    (SELECT min(timestamp) FROM read_parquet('history.parquet')) as min_date,
+    (SELECT max(timestamp) FROM read_parquet('history.parquet')) as max_date
+FROM parquet_metadata('history.parquet');
+
+SELECT pg_size_pretty(sum(total_compressed_size)::bigint) as size,
+    (SELECT count(*) FROM read_parquet('last-90-days.parquet')) as rows,
+    (SELECT min(timestamp) FROM read_parquet('last-90-days.parquet')) as min_date,
+    (SELECT max(timestamp) FROM read_parquet('last-90-days.parquet')) as max_date
+FROM parquet_metadata('last-90-days.parquet');
 
 ATTACH 'history.sqlite' AS sqlite_db (TYPE SQLITE);
 drop table if exists sqlite_db.spot_prices;
